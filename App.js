0c203ca495ac56ebf8fe132c1dfa503b36fed039
dev-song <52951039+dev-song@users.noreply.github.com>
2020-10-31 19:52:39 +0900

    Restore basic components and app structure #4

--- App.js
+++ App.js
@@ -11,103 +11,82 @@ import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
-  View,
   Text,
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Header from './components/Header';
+import CategoriesTabBar from './components/CategoriesTabBar';
+import SignInUpOverlay from './components/SignInUpOverlay';
+import SampleText from './components/SampleText';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      searchQuery: '',
+      tweets: [],
+    };
+  }
+
+  async getTweets(query = '#keyword') {
+    const url = `https://api.twitter.com/1.1/search/tweets.json?q=${query}&result_type=recent`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAK7LJAEAAAAA74VS32y9JjelRMgzoXyprCqf8%2Bs%3DRumh9ppa01bdYB2d7uHJhhViqlrleuZXXrvaRWBRnTSNs2C3Nd',
+        'content-type': 'application/json',
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  componentDidMount() {
+    this.getTweets();
+  }
+
+  render() {
+    return (
+      <>
+        <StatusBar />
+        <SafeAreaView style={styles.safeArea}>
           <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          <CategoriesTabBar />
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            contentContainerStyle={styles.scrollView}
+          >
+            <Text>List of Tweets</Text>
+            {new Array(8).fill(0).map((elm, index) => (
+              <SampleText key={index} />
+            ))}
+          </ScrollView>
+          <SignInUpOverlay />
+        </SafeAreaView>
+      </>
+    );
+  }
+}
+
+const APP_BG_COLOR = 'white';
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: APP_BG_COLOR,
   },
 });
 
---
+++ components/SampleText.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function SampleText() {
+  return (
+    <View>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+        necessitatibus incidunt provident vel voluptate id cumque eveniet
+        exercitationem non, ex explicabo rerum, aliquid dicta in eaque eius eius
+        eius consequatur voluptatibus tenetur vero deleniti expedita asperiores
+        nulla. Adipisci omnis laudantium magni.
+      </Text>
+    </View>
+  );
+}
+
+export default SampleText;
